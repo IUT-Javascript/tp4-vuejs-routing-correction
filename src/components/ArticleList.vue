6cc9590ca21fe3b647366d68708eef2290e8a55a
Alexandre Ruiz <alexandre.ruiz@apside.com>
2025-02-18 17:40:13 +0100

    Correction

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+import { ARTICLES_API } from '../const/config.js';
+import ArticleCard from './ArticleCard.vue';
+
+// const ARTICLES_API = 'https://6797ded3c2c861de0c6e4858.mockapi.io/articles';
+
+const articles = ref([]);
+getArticlesFromApi();
+
+function getArticlesFromApi(){
+  fetch(ARTICLES_API).then(response => {
+      response.json().then(json => {
+          json.forEach(a => {
+            const article = { id: a.id, title: a.title, description: a.description};
+            articles.value.push(article);
+        });
+      });
+    });
+}
+</script>
+
+<template>
+    <h3 v-if="articles.length">There are {{ articles.length }} articles</h3>
+
+    <ArticleCard v-if="articles.length" v-for="article in articles" 
+                    :key="article.title" 
+                    :article="article"/>  
+                    
+    <h3 v-else>
+        No articles
+    </h3>
+</template>
